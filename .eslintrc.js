fbe6ac59f6ff4e6b0cb6b038a5e49930241cb234
yangming <472102644@qq.com>
2019-05-05 15:47:57 +0800

    add ts and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: "standard",
+  plugins: [
+    "standard",
+    "promise",
+    "import",
+    "node"
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  }
+}
